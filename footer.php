9df4dbf52b3f9bc66873b3efb9f3beaba559489b
Adriano Guedes <adriano.sgs@gmail.com>
2022-05-09 19:22:21 -0300

    integrando o menu do site

--- footer.php
+++ footer.php
@@ -1,14 +1,15 @@
 <footer>
-			<nav class="nav-footer">
-				<ul>
-					<li class="current_page_item"><a href="/">Menu</a></li>
-					<li><a href="/Sobre/">Sobre</a></li>
-					<li><a href="/Contato/">Contato</a></li>
-				</ul>
-			</nav>
+  <nav class="nav-footer">
+    <ul>
+      <li class="current_page_item"><a href="/">Menu</a></li>
+      <li><a href="/Sobre/">Sobre</a></li>
+      <li><a href="/Contato/">Contato</a></li>
+    </ul>
+  </nav>
 
-			<p>Rest © 2015. Alguns direitos reservados.</p>
-		</footer>
-		<?php wp_footer(); ?>
-	</body>
+  <p>Rest © 2015. Alguns direitos reservados.</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
 </html>
--- header.php
+++ header.php
@@ -1,27 +1,28 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<title><?php bloginfo('name'); ?></title>
 
-		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-    <?php wp_head(); ?>
-	</head>
+<head>
+  <meta charset="utf-8">
+  <title><?php bloginfo('name'); ?></title>
 
-	<body>
-		
-		<header>
-			<nav>
-				<ul>
-					<li class="current_page_item"><a href="/">Menu</a></li>
-					<li><a href="/Sobre/">Sobre</a></li>
-					<li><a href="/Contato/">Contato</a></li>
-				</ul>
-			</nav>
+  <link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+  <?php wp_head(); ?>
+</head>
 
-			<h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
+<body>
 
-			<p>Rua Marechal 29 – Copacabana – Rj</p>
-			<p class="telefone">2422-9201</p>
-		</header>
+  <header>
+    <nav>
+      <ul>
+        <li class="current_page_item"><a href="/">Menu</a></li>
+        <li><a href="/Sobre/">Sobre</a></li>
+        <li><a href="/Contato/">Contato</a></li>
+      </ul>
+    </nav>
+
+    <h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
+
+    <p>Rua Marechal 29 – Copacabana – Rj</p>
+    <p class="telefone">2422-9201</p>
+  </header>

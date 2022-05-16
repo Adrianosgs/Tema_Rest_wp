a488943eb66b6928d4da6529b2de77812b0fa521
Adriano Guedes <adriano.sgs@gmail.com>
2022-05-16 19:13:54 -0300

    footer finalizado

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
 <footer>
   <nav class="nav-footer">
     <ul>
-      <li class="current_page_item"><a href="/">Menu</a></li>
-      <li><a href="/Sobre/">Sobre</a></li>
-      <li><a href="/Contato/">Contato</a></li>
-    </ul>
+      <?php
+	$args = array(
+		'menu' => 'principal',
+		'container' => false
+	);
+	wp_nav_menu( $args );
+?>
   </nav>
 
   <p><?php bloginfo('name'); ?> © <?php echo date("Y"); ?>. Alguns direitos reservados.</p>
